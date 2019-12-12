f8cd6f5a4aa7c8c553532de4b2b724244580ea34
Alexa Sanchez <alexsc167@gmail.com>
2019-12-12 20:19:30 +0000

    adding the logo to the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
 
 class Header extends React.Component<{}, {}> {
   render() {
     return (
       <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" />
+        </div>
         <Link to="/login">
           <button className={styles.login}>Sign In</button>
         </Link>
