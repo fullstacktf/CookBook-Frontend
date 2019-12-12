708a8c4833222cf6f70ec9a69576e6af6da318e6
frannav <frannavmart@gmail.com>
2019-12-12 16:53:12 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RecipeCard from './components/recipeCard/recipeCard';
+
+const App: React.FC = () => {
+  return (
+    <div>
+          <RecipeCard></RecipeCard>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/recipeCard/__tests__/recipeCard.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+// describe('recipeCardtest', () => {
+//     describe('render', () => {
+//         test('Should render a component type ', () => {
+//             const wrapper = shallow(<recipeCardtest/>);
+//         });
+//     });
+// });
