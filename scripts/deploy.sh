da492bff365a065121645efa7eaa0e1e871caf5f
frannav <frannavmart@gmail.com>
2019-12-13 19:55:57 +0000

    changed travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -i deploymentCookBook -v -r public ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+scp -o StrictHostKeyChecking=no -i deploymentCookBook -v -r build ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
 #COMMAND="cd ${REMOTE_PATH} && ls -la"
 
