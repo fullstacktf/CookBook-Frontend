6f2584b7d5f04ddcdb3a6565fc5f29d88a6061ad
Carlos Arvelo <carvelog@gmail.com>
2019-12-14 00:21:19 +0000

    travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 scp -o StrictHostKeyChecking=no -i deploymentCookBook -v -r build ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
-
-#COMMAND="cd ${REMOTE_PATH} && ls -la"
-
-#ssh -o StrictHostKeyChecking=no -i deploymentCookBook -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
