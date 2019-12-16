bce3f2a57a4e74ff7e4fa7a4014fa03c3b51bab2
Carlos Arvelo <carvelog@gmail.com>
2019-12-16 16:12:18 +0000

    changed deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+COMMAND="rm -rf ${REMOTE_PATH}/* "
+
+ssh -o StrictHostKeyChecking=no -i deploymentCookBook -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
 scp -o StrictHostKeyChecking=no -i deploymentCookBook -v -r build/* ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
