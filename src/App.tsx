499e9b2904d3cef428a99deb6a91431a2127c901
Carlos Arvelo <carvelog@gmail.com>
2019-12-18 17:53:24 +0000

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter  as Router, Route} from "react-router-dom"
- 
 
 //rutas
 import Log from './pages/Log';
@@ -11,10 +10,9 @@ class App extends React.Component<{},{}> {
   render() {
   return (
     <Router> 
-      <Route path="/login" component={Log}/>
-      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Log} />
+      <Route exact path="/" component={Home} />
     </Router>
-   
   );  
 }
 }
